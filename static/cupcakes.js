af0f7c06afaad4f6b2d694911b6610ad555dc69e
DanielleM <dlmedeiro@gmail.com>
2022-08-11 18:00:20 -0700

    update missing https

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -15,7 +15,7 @@ function cupcakeHtml(cupcake) {
 }
 
 async function getCupcakes() {
-    const response = await axios.get("http://127.0.0.1:5000/api/cupcakes");
+    const response = await axios.get("https://127.0.0.1:5000/api/cupcakes");
 
     for (let cupcake of response.data.cupcakes) {
         let lineItem = $(cupcakeHtml(cupcake));
@@ -33,7 +33,7 @@ $("#newCupcakeForm").on("submit", async function (e){
     let rating = $("#rating").val();
     let image = $("#image").val();
     
-    const newCupcakeResponse = await axios.post("http://127.0.0.1:5000/api/cupcakes", {flavor, size, rating, image});
+    const newCupcakeResponse = await axios.post("https://127.0.0.1:5000/api/cupcakes", {flavor, size, rating, image});
     
     let newCupcake = $(cupcakeHtml(newCupcakeResponse.data.cupcake));
     $("#cupcake-list").append(newCupcake);
@@ -45,7 +45,7 @@ $("#cupcake-list").on("click", ".delete-button", async function (e){
     let $cupcake = $(e.target).closest("div");
     let cupcakeId = $cupcake.attr("data-cupcake-id");
 
-    await axios.delete(`http://127.0.0.1:5000/api/cupcakes/${cupcakeId}`)
+    await axios.delete(`https://127.0.0.1:5000/api/cupcakes/${cupcakeId}`)
     $cupcake.remove();
 });
 
